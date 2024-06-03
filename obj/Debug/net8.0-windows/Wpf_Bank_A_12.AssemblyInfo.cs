25756c55c44253b9984f00c4f086e0d1a4c2219d
Matin <shlembert@yandex.ru>
2024-06-03 12:16:28 +0300

    hz

--- obj/Debug/net8.0-windows/Wpf_Bank_A_12.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Wpf_Bank_A_12.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wpf_Bank_A_12")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1996a6dfdcbdabb04c72cebbd93568f07505222")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6439a38c0ffc4724e95a3fad1d17554ceca91478")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wpf_Bank_A_12")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wpf_Bank_A_12")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
